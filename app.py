0ae329bc836086b90faa02f02224a00ff5b39195
RoshanAswal <73030476+RoshanAswal@users.noreply.github.com>
2022-02-10 18:03:55 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from crypt import methods
+from flask import Flask, render_template,request
+import numpy as np
+import pickle 
+
+app=Flask(__name__)
+
+model=pickle.load(open("model.pkl",'rb'))
+cv=pickle.load(open("cv.pkl",'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=["POST","GET"])
+def check():
+    instance=str(request.form['text_box'])
+    vec=cv.transform([instance])
+    prediction=model.predict(vec)
+    result=""
+    if prediction[0]==1:
+        result="Offensive"
+    else:
+        result="Non-Offensive"
+    return render_template("index.html",answer=result)
+
+if __name__=='__main__':
+    app.run(debug=True)
